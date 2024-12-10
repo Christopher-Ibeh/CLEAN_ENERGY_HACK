20a47edcbde30466fbcaf8c379944cc5a68622bd
Christopher Ibeh <cuibeh@gmail.com>
2024-12-10 01:05:41 +0000

    Added features

--- Clean_p.py
+++ Clean_p.py
@@ -2,6 +2,21 @@
 ##CLEAN POWER HACK
 ##################
 
-
+#%%
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt  # For plotting and visualization
+import seaborn as sns       # For advanced visualization
+import requests             # For making HTTP requests
+import os                   # For operating system interfaces
+import sys                  # For system-specific parameters and functions
+
+#ML
+from sklearn.model_selection import train_test_split  # For splitting datasets
+from sklearn.ensemble import RandomForestClassifier  # Example classifier
+from sklearn.metrics import accuracy_score           # For evaluating models
+import tensorflow as tf                              # For deep learning
+import keras 
+
+
+# %%
